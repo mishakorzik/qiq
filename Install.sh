4bea88d04d48713fd3c2193f61a559da32f06522
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-14 20:56:28 +0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,21 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+echo -e $b">>>"$w" developer: "$g"mishakorzhik"$w
+echo -e $b">>>"$w" installing: "$g"qiq = v0.21"$w
+cd
+cd
+apt install figlet
+gem install lolcat
+cd qiq
+rm -rf README.md
+rm -rf LICENSE
+chmod +x *
+cp * /data/data/com.termux/files/usr/bin/
+figlet -f big 'Done!!!' | lolcat -p 1.0
+echo -e $b">>>"$w" run command to help qiq: "$g"qiq-help"$w
+sleep 1
+echo -e $b">>>"$w" Trank for installing!: "$g"qip"$w
+echo -e $b">>>"$w" Bye bye! Have a nice day!: "$g"qip"$w
