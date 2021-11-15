3e04d192e0dfa403c5735f213336d067cfed654b
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-11-15 21:12:06 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,59 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+
+echo -e $b">>>"$w" developer: "$g"mishakorzhik"$w
+echo -e $b">>>"$w" installing: "$g"qiq = v1.22"$w
+cd
+cd
+pkg install ruby
+apt install figlet
+gem install lolcat
+pkg install neofetch
+apt install neofetch
+pkg install mc
+cd qiq
+rm -rf README.md
+rm -rf LICENSE
+chmod +x *
+cp qiq-help /data/data/com.termux/files/usr/bin/
+cp qiq-version /data/data/com.termux/files/usr/bin/
+cp qiq-author /data/data/com.termux/files/usr/bin/
+cp qiq-install /data/data/com.termux/files/usr/bin/
+cp qiq-remove /data/data/com.termux/files/usr/bin/
+cp qiq-reinstall /data/data/com.termux/files/usr/bin/
+cp qiq-update /data/data/com.termux/files/usr/bin/
+cp delete-qiq /data/data/com.termux/files/usr/bin/
+cp Rootkite-delete-qiq /data/data/com.termux/files/usr/bin/
+cp qiq-proces /data/data/com.termux/files/usr/bin/
+cp qiq-bigtext /data/data/com.termux/files/usr/bin/
+cp qiq-smalltext /data/data/com.termux/files/usr/bin/
+cp qiq-mysystem /data/data/com.termux/files/usr/bin/
+cp qiq-rcolor /data/data/com.termux/files/usr/bin/
+cp qiq-rgb /data/data/com.termux/files/usr/bin/
+cp qiq-home /data/data/com.termux/files/usr/bin/
+cp qiq-usr /data/data/com.termux/files/usr/bin/
+cp qiq-bin /data/data/com.termux/files/usr/bin/
+cp qiq-netstatus /data/data/com.termux/files/usr/bin/
+cp qiq-mcolor /data/data/com.termux/files/usr/bin/
+cp qiq /data/data/com.termux/files/usr/bin/
+cp qiq-install-py /data/data/com.termux/files/usr/bin/
+cp qiq-dispatcher /data/data/com.termux/files/usr/bin/
+cp qiq-mem /data/data/com.termux/files/usr/bin/
+cp qiq-cpu /data/data/com.termux/files/usr/bin/
+cp qiq-ping /data/data/com.termux/files/usr/bin/
+cp qiq-directory /data/data/com.termux/files/usr/bin/
+cp qiq-packets /data/data/com.termux/files/usr/bin/
+cp qiq-manager /data/data/com.termux/files/usr/bin/
+cd
+cd
+cd qiq
+cd .fonts
+chmod +x *
+cp * /data/data/com.termux/files/usr/share/figlet/
+figlet -f big 'Done!!!' | lolcat -p 1.0
+echo -e $b">>>"$w" run command to help qiq: "$g"qiq-help"$w
+sleep 1
+echo -e $b">>>"$w" Trank for installing!: "$g"qiq"$w
+echo -e $b">>>"$w" Bye bye! Have a nice day!: "$g"qiq"$w
